e8dc934c170bf64f97c9ba20dba423685372eaf2
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 00:35:13 +0530

    Welcome to DataStrctures

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresDay17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The DataStructures !");
+        }
+    }
+}
